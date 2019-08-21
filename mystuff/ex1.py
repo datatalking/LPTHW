961cc83ffc902db08ef11f4df466a18ec2699f85
Andrew Schell <andrewschell3@gmail.com>
2019-08-20 19:27:04 -0700

    first commit ex1 to ex5

---
+++ mystuff/ex1.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Hello Again")
+print("I like typing it is fun")
+print("This is fun")
+print('yippy skippy')
+print('Yay! Printing')
+print("I'd much rather you not")
+print('I "said" do not touch this')
