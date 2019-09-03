99256dd45e03ce667856dd44585d8dc6c71248ee
Andrew Schell <andrewschell3@gmail.com>
2019-09-03 16:17:35 -0700

    ex13.py added comments

--- mystuff/ex13.py
+++ mystuff/ex13.py
@@ -1,10 +1,10 @@
 # PATH ~/sbox/lpthw/mystuff/ex13.py
 
-from sys import argv
+from sys import argv # imports argv from sys and not the whole sys module
 
-script, first, second, third = argv
+script, first, second, third = argv # sets variables as argv
 
-print("The script is called:", script)
-print("Your first variable is:", first)
-print("Your second variable is:", second)
-print("Your third variable is:", third)
+print("The script is called:", script) # prints str followed by var as argv
+print("Your first variable is:", first) # prints str followed by var as argv
+print("Your second variable is:", second) # prints str followed by var as argv
+print("Your third variable is:", third) # prints str followed by var as argv
