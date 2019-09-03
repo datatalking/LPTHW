7b4d3c543fee2ea11a3ae0296fe5bd400f81d76d
Andrew Schell <andrewschell3@gmail.com>
2019-09-02 18:39:10 -0700

    adding ex11.py

---
+++ mystuff/ex11.py
@@ -0,0 +1,11 @@
+# path = sbox/lpthw/mystuff/ex11.py
+
+print("How old are you",)
+age = input()
+print("How tall are you",)
+height = input()
+print("how much do you weight",)
+weight = input()
+
+print("So you're %r old, %r tall and %r heavy." % (age, height, weight))
+# so I ran this from sublime and it didnt run, but it does run from terminal
